21b203fd87e81bc62ed71cdae4ccabc541695fb9
Mustafa Gedik <mstf.gdik19@gmail.com>
2021-08-31 21:33:44 +0300

    Add files via upload

---
+++ Ders03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ders03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Birinci sayıyı girin: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("İkinci sayıyı girin: ");
+                int b = int.Parse(Console.ReadLine());
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
