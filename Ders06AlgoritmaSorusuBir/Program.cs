4d6e72adb9068c3fe437e2b34adbaa04e9e64084
Mustafa Gedik <mstf.gdik19@gmail.com>
2021-09-01 22:46:36 +0300

    Add files via upload

---
+++ Ders06AlgoritmaSorusuBir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ders06AlgoritmaSorulari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi_N = 0, sayac = 0;
+            int[] dizi = new int[sayi_N];
+
+            System.Console.WriteLine("Kaç sayı girmek istiyorsunuz?: ");
+            sayi_N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                sayac++;
+                System.Console.WriteLine(sayac + ". pozitif sayıyı giriniz: ");
+                dizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var dz in dizi)
+            {
+                if(dz % 2 == 0)
+                    System.Console.Write(dz + ", ");
+            }
+        }
+    }
+}
---
+++ Ders06AlgoritmaSorusuIki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ders06AlgoritmaSorusuIki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int sayi_N = 0, sayac = 0, sayi_M = 0;
+
+            System.Console.WriteLine("Kaç sayı girmek istiyorsunuz?: ");
+            sayi_N = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Bölünecek sayıyı girin: ");
+            sayi_M = int.Parse(Console.ReadLine());
+
+            int[] dizi = new int[sayi_N];
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                sayac++;
+                System.Console.WriteLine(sayac + ". pozitif sayıyı giriniz: ");
+                dizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var dz in dizi)
+            {
+                if((dz % sayi_M == 0) || (dz == sayi_M))
+                    System.Console.Write(dz + ", ");
+            }
+        }
+    }
+}
